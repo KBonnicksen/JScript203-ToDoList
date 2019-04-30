beacd58f80526623cd545da87327917ac7865228
KELSEY Bonnicksen <bonnicksen8017@students.cptc.edu>
2019-04-30 13:51:42 -0700

    Created ToDoItem class

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,3 +1,22 @@
 window.onload = functon{
 
 }
+
+class ToDoItem{
+    title:string;
+    description:string;
+    startDate:Date;
+    endDate:Date;
+    isComplete:boolean;
+    urgency:string;
+}
+
+let testItem = new ToDoItem();
+testItem.title = "Go to class";
+testItem.description = "Wake up, get ready, drive and listen to podcast";
+testItem.startDate = new Date("April 30, 2019");
+testItem.endDate = new Date("April 30, 2019");
+testItem.isComplete = false;
+if(testItem.isComplete){
+    //do something
+}
